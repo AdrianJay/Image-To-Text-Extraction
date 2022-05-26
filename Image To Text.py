a6f78d70222099c9e27d655152319013d3f22ade
AdrianJay <96049530+AdrianJay@users.noreply.github.com>
2022-05-26 23:49:51 +0800

    Update Image To Text.py

--- Image To Text.py
+++ Image To Text.py
@@ -8,7 +8,15 @@ import cv2
 
 print("Type filename")
 filename = input(":")
-myconfig = r"==psm 2 ==oem 3"
+print(" ")
+print("Type psm (from 0-13)")
+PSM = int(input(":"))
+print(" ")
+print("Type oem (0-3)")
+OEM = int(input(":"))
+print(" ")
+
+myconfig = r"==psm PSM ==oem OEM"
 
 text = pytesseract.image_to_string(PIL.Image.open(filename), config=myconfig)
 #print(logos)
@@ -25,7 +33,7 @@ for i in range(amount_boxes):
     if float(data['conf'][i]) > 80:
         (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
         img = cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
-        img = cv2.putText(img, data['text'][i], (x, y + height + 20 ), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+        img = cv2.putText(img, data['text'][i], (x, y + height + 20 ), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
 cv2.imshow("image", img)
 cv2.waitKey(0)
