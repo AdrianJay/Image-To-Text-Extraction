a621142f7bc9d5f208932d3c6517f752674aa978
francisosano <95964380+francisosano@users.noreply.github.com>
2022-05-26 23:15:37 +0800

    Update Image To Text.py

--- Image To Text.py
+++ Image To Text.py
@@ -5,3 +5,17 @@ import pytesseract
 from pytesseract import Output
 import PIL.Image
 import cv2
+
+print("Type filename")
+filename = input(":")
+myconfig = r"==psm 2 ==oem 3"
+
+text = pytesseract.image_to_string(PIL.Image.open(filename), config=myconfig)
+#print(logos)
+
+img = cv2.imread(filename)
+height, width, channels = img.shape
+
+data = pytesseract.image_to_data(img, config = myconfig, output_type = Output.DICT)
+
+#print(data['logos'])
