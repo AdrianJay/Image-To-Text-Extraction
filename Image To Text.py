d03c6d3990b247e87551cdd896f0f982626fb71e
Harold <96050454+haroldgumafelix@users.noreply.github.com>
2022-05-26 23:33:39 +0800

    Update Image To Text.py

--- Image To Text.py
+++ Image To Text.py
@@ -19,3 +19,13 @@ height, width, channels = img.shape
 data = pytesseract.image_to_data(img, config = myconfig, output_type = Output.DICT)
 
 #print(data['logos'])
+
+amount_boxes = len(data['text'])
+for i in range(amount_boxes):
+    if float(data['conf'][i]) > 80:
+        (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+        img = cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
+        img = cv2.putText(img, data['text'][i], (x, y + height + 20 ), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+
+cv2.imshow("image", img)
+cv2.waitKey(0)
